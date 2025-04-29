58a83e17e77c678ca95a9081077575da8d670e7f
Yeonwoo Ji <71367047+ywoo121@users.noreply.github.com>
2025-04-29 14:41:27 +0900

    Chore: Connect MySQL DB (#2) (#3)

---
+++ Koco/src/main/java/icet/koco/KocoApplication.java
@@ -0,0 +1,13 @@
+package icet.koco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KocoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KocoApplication.class, args);
+	}
+
+}
---
+++ Koco/src/test/java/icet/koco/KocoApplicationTests.java
@@ -0,0 +1,13 @@
+package icet.koco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KocoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
