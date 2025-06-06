533764c91e608a26600ae99054f6b4f0a88ced9d
ywoo121 <71367047+ywoo121@users.noreply.github.com>
2025-06-06 20:48:32 +0900

    Docs: Swagger 설명 추가

--- Koco/src/main/java/icet/koco/posts/controller/PostController.java
+++ Koco/src/main/java/icet/koco/posts/controller/PostController.java
@@ -4,6 +4,9 @@ import icet.koco.enums.ApiResponseCode;
 import icet.koco.global.dto.ApiResponse;
 import icet.koco.posts.dto.PostCreateRequestDto;
 import icet.koco.posts.service.PostService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.Table;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,10 +18,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/api/backend/v3/posts")
 @RequiredArgsConstructor
+@Tag(name = "Posts", description = "게시판 관련 API들 입니다.")
 public class PostController {
     private final PostService postService;
 
     @PostMapping
+    @Operation(summary = "게시글을 등록하는 API입니다.")
     public ResponseEntity<?> createPost(@RequestBody PostCreateRequestDto requestDto) {
         Long userId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 
