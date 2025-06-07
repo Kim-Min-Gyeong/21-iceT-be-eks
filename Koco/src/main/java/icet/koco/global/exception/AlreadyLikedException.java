cefd8af3176f2cfd1cbe04ab77a35645e88219a3
ywoo121 <71367047+ywoo121@users.noreply.github.com>
2025-06-08 01:09:11 +0900

    Feat: AlreadyLikedException 커스텀 예외

---
+++ Koco/src/main/java/icet/koco/global/exception/AlreadyLikedException.java
@@ -0,0 +1,7 @@
+package icet.koco.global.exception;
+
+public class AlreadyLikedException extends RuntimeException {
+    public AlreadyLikedException(String message) {
+        super(message);
+    }
+}
