d8d7c42a150d6ca08d127952339e3e0ff865647c
ywoo121 <71367047+ywoo121@users.noreply.github.com>
2025-06-15 16:14:33 +0900

    Chore: 유저 탈퇴 시 사용자별 유저 알고리즘 스탯 & 설문 기록 삭제

--- Koco/src/main/java/icet/koco/problemSet/dto/ProblemSurveyRequestDto.java
+++ Koco/src/main/java/icet/koco/problemSet/dto/ProblemSurveyRequestDto.java
@@ -1,10 +1,14 @@
 package icet.koco.problemSet.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProblemSurveyRequestDto {
     private Long problemId;
     private boolean isSolved;
